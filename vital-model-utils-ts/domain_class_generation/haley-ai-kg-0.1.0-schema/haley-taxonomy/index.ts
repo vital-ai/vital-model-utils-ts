f346d6b929e77e58847b8e20981224430de2cb3a
Marc Hadfield <marc@vital.ai>
2025-10-16 14:33:32 -0400

    Remove generated domain_class_generation files - these should not be tracked

--- vital-model-utils-ts/domain_class_generation/haley-ai-kg-0.1.0-schema/haley-taxonomy/index.ts
+++
@@ -1,6 +0,0 @@
-// Haley Taxonomy ontology classes
-export { TaxonomyNode } from './taxonomyNode.js';
-export { Edge_hasBroaderCategory } from './edge_hasBroaderCategory.js';
-export { Edge_hasEquivalentCategory } from './edge_hasEquivalentCategory.js';
-export { Edge_hasNarrowerCategory } from './edge_hasNarrowerCategory.js';
-export { Edge_hasRelatedCategory } from './edge_hasRelatedCategory.js';
